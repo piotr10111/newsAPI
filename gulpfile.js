0a5760e56a768b26fed1948d20541a3ebf03a91c
Piotr <piotr.michalski2000@gmail.com>
2018-10-19 13:29:01 +0200

    Working scripts, added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const htmlmin = require("gulp-htmlmin");
+const autoprefixer = require('gulp-autoprefixer');
+
+sass.compiler = require('node-sass');
+
+const dist = 'dist'
+
+gulp.task('sass',  function() {
+    return gulp.src('./src/scss/*.scss')
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(autoprefixer())
+      .pipe(concat('style.css'))
+      .pipe(gulp.dest(dist+'/css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./src/scss/*.scss', ['sass']);
+});
+
+gulp.task('js', function(){
+    return gulp.src('./src/scripts/*.js') 
+    .pipe(uglify())
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(dist+'/js'));
+});
+
+gulp.task('html', function() {
+    return gulp.src('*.html')
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(gulp.dest(dist));
+  });
+
+gulp.task('default', [ 'sass:watch','sass','html'])
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,10 +6,81 @@ const apis = [
     `https://newsapi.org/v2/top-headlines?country=pl&category=business&entertainment&apiKey=0f1d6f5ab7564ab09bc63df2a2fc6dcb`
 ];
 
-Promise.all(apis.map(url =>
+/*Promise.all(apis.map(url =>
     fetch(url)
         .then(resp => resp.json())
         .then(data => {
-            console.log(data)
-    })));
+            console.log(data);
+})));*/
+
+const reqFirst = fetch(apis[0])
+    .then(resp => {
+        return resp.json()
+    })
+    .then(data => {
+        console.log(data)
+        const news = data.articles;
+        
+        news.map( article => {
+             const post = document.querySelector('.articles');
+            post.innerHTML += `
+                <article class="article">
+                    <figure> 
+                        <a href="${article.url}">
+                            <img src="${article.urlToImage}" alt="">
+                        </a>
+                    </figure>
+                    <header>
+                        <a href="${article.url}" 
+                            <h3 class="article-title">${article.title}</h3>
+                        </a>
+                        <a class="article-author" rel="author">Author: ${article.author}</a>
+                    </header>
+                    <p class="article-description">Description: ${article.description}</p>
+                </article>`;
+        });
+    });
+
+
+const reqSecond = fetch(apis[1])
+    .then(resp => {
+        return resp.json()
+    })
+    .then(data => console.log(data));
+
+const reqThird = fetch(apis[2])
+    .then(resp => {
+        return resp.json()
+    })
+    .then(data => console.log(data));
+
+const reqFourth = fetch(apis[3])
+    .then(resp => {
+        return resp.json()
+    })
+    .then(data => console.log(data));
+
+const reqFifth = fetch(apis[4])
+    .then(resp => {
+        return resp.json()
+    })
+    .then(data => console.log(data));
+
+const sourceData = {
+    "reqFirst": {},
+    "reqSecond": {},
+    "reqThird": {},
+    "reqFourth": {},
+    "reqFifth": {}
+};
+
+Promise.all([reqFirst, reqSecond, reqThird, reqFourth, reqFifth]).then(values => {
+    sourceData["reqFirst"] = values[0];
+    sourceData["reqSecond"] = values[1];
+    sourceData["reqThird"] = values[2];
+    sourceData["reqFourth"] = values[3];
+    sourceData["reqFifth"] = values[4];
+    return sourceData;
+});
+
 
