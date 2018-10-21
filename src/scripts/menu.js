628673df3811ff09b2a3025215e69b7012a62e35
Piotr <piotr.michalski2000@gmail.com>
2018-10-21 22:56:03 +0200

    menu works okv2

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,32 +1,12 @@
-/*const hamburger = document.querySelector('.nav-btn');
-const ul = document.querySelector('.header__nav');
-const  = document.querySelector('.nav__list');
-const click = hamburger.addEventListener('click', function (event) {
-    event ? event : event = window.event;
-    event.preventDefault();
-    hamburger.classList.toggle('header__button--close');
-
-});*/
-
-/*
-const hide = function hide() {
-    ul.style.visibility = "hidden";
-    hamburger.classList.remove('header__button--close');
-};
-
-
-links.forEach(function (link) {
-    return link.addEventListener('click', hide);
-});*/
-const trigger = document.querySelector(".menu-icon");
-const menu = document.querySelector(".menu");
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".header__menu");
 
 function toggleClass() {
-	trigger.classList.toggle('active');
+	hamburger.classList.toggle('active');
 	menu.classList.toggle('active')
 }
 
-trigger.addEventListener('click', toggleClass);
+hamburger.addEventListener('click', toggleClass);
 menu.addEventListener('click', toggleClass);
 window.addEventListener('keyup', function(e){
 	if (menu.classList.contains('active') && e.keyCode === 27) {
